cad298a56bd1b3949ce9826be64e1feaa9572ac5
Fake Name <eah1759@g.coppellisd.com>
2016-12-08 16:31:15 -0600

    Ignore and Deny this
    I don't understand GitHub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutomTrialCode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+/**
+ * Created by eah1759 on 12/8/2016.
+ */
+
+public class AutomTrialCode {
+}
